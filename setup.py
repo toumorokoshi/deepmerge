edc6b808d931a939aac628353a5f95e97586e31f
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-11-30 19:59:41 -0800

    shipped v0.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 tests_require = []
 
 setup(name='deepmerge',
-      version='0.0.2',
+      version='0.0.4b',
       description='a toolset to deeply merge python dictionaries.',
       long_description=open(README_PATH).read(),
       author='Yusuke Tsutsumi',
