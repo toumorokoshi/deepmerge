1295c999e63e47905dc6754da9ac3530bcbb4163
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-02-16 14:11:06 -0800

    minor: updating ubuild to use twine.

--- ubuild.py
+++ ubuild.py
@@ -11,25 +11,33 @@ def test(build):
     build.packages.install("pytest")
     build.packages.install("pytest-cov")
     pytest = os.path.join(build.root, "bin", "py.test")
-    subprocess.call([
-        pytest, "--cov", "deepmerge",
-        "deepmerge/tests",
-        "--cov-report", "term-missing"
-    ] + build.options.args)
+    subprocess.call(
+        [
+            pytest,
+            "--cov",
+            "deepmerge",
+            "deepmerge/tests",
+            "--cov-report",
+            "term-missing",
+        ]
+        + build.options.args
+    )
 
 
 def publish(build):
     """ distribute the uranium package """
     build.packages.install("wheel")
-    build.executables.run([
-        "python", "setup.py",
-        "sdist", "bdist_wheel", "--universal", "upload", "--release"
-    ])
+    build.packages.install("twine")
+    # to use keyring
+    build.packages.install("keyring")
+    build.packages.install("python3-dbus")
+    build.executables.run(
+        ["python", "setup.py", "sdist", "bdist_wheel", "--universal", "--release"]
+    )
+    build.executables.run(["twine", "upload", "dist/*"])
 
 
 def build_docs(build):
     build.packages.install("sphinx")
     build.packages.install("sphinx_rtd_theme")
-    return subprocess.call(
-        ["make", "html"], cwd=os.path.join(build.root, "docs")
-    )
+    return subprocess.call(["make", "html"], cwd=os.path.join(build.root, "docs"))
